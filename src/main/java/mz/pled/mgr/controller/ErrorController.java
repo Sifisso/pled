09e35d4be903df1d7435b159620fc97ee76aae4f
APOIADOR <imatata16@gmail.com>
2022-07-05 12:06:24 -0700

    Actualização dos dizeres para página de Erro

--- src/main/java/mz/pled/mgr/controller/ErrorController.java
+++ src/main/java/mz/pled/mgr/controller/ErrorController.java
@@ -2,11 +2,14 @@ package mz.pled.mgr.controller;
 
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
+@Component
 public class ErrorController implements ErrorViewResolver {
     @Override
     public ModelAndView resolveErrorView(HttpServletRequest request, HttpStatus status, Map<String, Object> map) {
@@ -16,7 +19,7 @@ public class ErrorController implements ErrorViewResolver {
         switch(status.value()){
             case 404:
                 model.addObject("error","Página não Encontrada!!");
-                model.addObject("message","O Endereço URL para a página: '"+ map.get("path") + "'Não foi Existe.");
+                model.addObject("message","O Endereço URL para a página: '"+ map.get("path") + "' Não foi Existe.");
                 break;
             case 500:
                 model.addObject("error","Ocorreu um erro interno no Servidor!!!");
